8da81da4e09d147a368208c7ed3b9dc869b809da
Lauren <TechnologicalType@MacBook-Air-2.attlocal.net>
2019-12-31 19:47:45 -0500

    updated api for the yoda tranlastion and the id associated

--- assets/js/api/yoda.js
+++ assets/js/api/yoda.js
@@ -1,5 +1,5 @@
 $('#but').click( function() {
-    var input = document.getElementById('input').value;
+    var input = document.getElementById('inputYoda').value;
     $.getJSON("https://api.funtranslations.com/translate/yoda.json?text=" + input, function( data ) {
        $('#result').append(data.contents.translated);
     });
